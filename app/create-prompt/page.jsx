255f190f49b2277bedc6b6a0730f9e0e3c87ace4
Peter Japhet <peterjaphet111@gmail.com>
2023-09-03 05:27:49 +0300

    added Prompt Form

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -14,7 +14,32 @@ const [post,setPost] = useState({
     tag:'',
 })
 
+const router = useRouter();
+const { data: session } = useSession();
+
 const createPrompt = async (e) =>{
+    e.preventDefault()
+
+    setSubmitting(true);
+
+    try {
+        const res = await fetch('api/prompt/new',{
+            method: 'POST' ,  
+            body: JSON.stringify({
+                prompt: post.prompt,
+                userId:session?.user.id,
+                tag: post.tag
+            })
+        })
+
+        if(res.ok){
+            router.push('/')
+        }
+    } catch (error) {
+        console.log(error)
+    }finally {
+        setSubmitting(false)
+    }
 }
 
 
